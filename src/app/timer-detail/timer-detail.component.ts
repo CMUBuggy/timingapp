e3c88f765f42bf62568d5ab518933b44ec8c4e1b
Rob Siemborski <robsiemb@gmail.com>
2023-11-14 08:50:18 -0500

    Minor buggy timing ui changes to make your actions clearer.

--- src/app/timer-detail/timer-detail.component.ts
+++ src/app/timer-detail/timer-detail.component.ts
@@ -12,6 +12,12 @@ export class TimerDetailComponent implements OnChanges {
   // Can't click because buggy unstarted and I am not a start location.
   // Always false if clickInvalid == true.
   clickTooEarly : boolean = false;
+  // Clicking will start the roll (valid at Start & Crosswalk when roll unstarted)
+  // Always false if clickInvalid == true.
+  clickToStart : boolean = false;
+  // Clikcing will enter a new time (valid when buggy has not passed current location and roll started)
+  // Always false if clickInvalid == true.
+  clickToTime : boolean = false;
 
   @Input() timer: ExtendedTimerDetail | null = null;
   @Input() myLocation: number = -1;
@@ -28,9 +34,19 @@ export class TimerDetailComponent implements OnChanges {
       this.clickTooEarly =
         this.timer != null &&
         (this.timer.lastSeenAt == null || this.timer.lastSeenAt < 0) &&
-        !([0, 2].includes(this.myLocation))
+        !([0, 2].includes(this.myLocation));
+      this.clickToTime =
+        this.timer != null &&
+        (this.timer.lastSeenAt != null && this.timer.lastSeenAt >= 0) &&
+        (this.myLocation != 0);
+      this.clickToStart =
+        this.timer != null &&
+        (this.timer.lastSeenAt == null || this.timer.lastSeenAt < 0) &&
+        ([0, 2].includes(this.myLocation));
     } else {
       this.clickTooEarly = false;
+      this.clickToTime = false;
+      this.clickToStart = false;
     }
   }
 }
